e647f634727cbf67aba65589b14e45c3d5efc0ba
stssmlr <stanislav.smolyar@gmail.com>
2024-09-29 15:26:08 +0300

    Create User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserCardComponent } from "./user-card/user-card.component";
 import { IUser, USERS } from './user';
+import { UserFormComponent } from "./user-form/user-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent],
+  imports: [RouterOutlet, UserCardComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Angular-PV212';
   name = "Vlad Tymoshchuk";
@@ -20,12 +22,18 @@ export class AppComponent {
   changeName() {
     this.name += '!';
   }
+  createUser(user: IUser) {
+    this.users.push(user);
+  }
 
+  
   clearAll() {
     this.users = [];
   }
 
   deleteItem(id: number){
     let index = this.users.findIndex(x=> x.id === id)
+    if (index !== -1)
+      this.users.splice(index, 1);
   }
 }
